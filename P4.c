d025534fc172ae4c744c90379113698faae501cb
Iasmin Martins <42560173+iasminmartins@users.noreply.github.com>
2018-11-15 14:21:32 -0200

    Add files via upload

---
+++ P4.c
@@ -0,0 +1,26 @@
+/* P4.c */
+/* Iasmin Martins */
+/* 11811EBI003 */
+
+#include <stdio.h>
+
+double funcao(float, float);
+
+int main()
+{
+	float m, n;
+	printf("Entre com dois numeros: ");
+	scanf ("%f + %f", &m, &n);
+	printf("%g + %g = %g", m, n, funcao(m,n));
+	return 0;
+}
+
+double funcao(float m, float n)
+{
+	if(m == 0) 
+		return n + 1;
+	else if(m > 0 && n == 0) 
+		return funcao(m - 1, 1);
+	else if(m > 0 && n > 0) 
+		return funcao(m - 1, funcao(m, n - 1));
+}
