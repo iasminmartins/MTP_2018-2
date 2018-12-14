d65f578e30c4f79616e6ae331513d18dc412f40a
Iasmin Martins <42560173+iasminmartins@users.noreply.github.com>
2018-12-14 13:19:04 -0200

    Update P3.c

--- P3.c
+++ P3.c
@@ -5,18 +5,17 @@
 #include <stdio.h>
 
 int main(){
-	char numero[256], n[256];
+	char numero[256];
 	int i = 0, j = 0;
 	printf("Entre com uma frase: ");
 	scanf("%s", numero);
 	while(numero[i] != '\0'){
 		if(numero[i] >= 48 && numero[i] <= 57){
-			n[j] = numero[i];
-			j++;
+			j = j * 10 + (numero[i] - '0');
 		}
 		i++;
 	}
-	i = atoi(n);
-	printf("%d", i);
+	printf("%d", j);
 	return 0;
 }
+	
