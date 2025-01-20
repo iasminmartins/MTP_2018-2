0f5b2cdc402adf1cd09849c76684d5ff181f1db3
Iasmin Martins <42560173+iasminmartins@users.noreply.github.com>
2025-01-19 21:02:31 -0300

    style: reformat code and translate to English

--- P0.c
+++ P0.c
@@ -2,16 +2,24 @@
 /* iasmin */
 /* 11811EBI003 */
 #include <stdio.h>
-int main(int argc, char ** argv) {
-   int i;
-   if(argc > 1){
-if(argc > 2)
-   for(i = 0; i < argv[2][0]-'0'; i++)
-printf("Valeu, %s\n", argv[1]);
-else
-    printf("Saudacoes, %s\n", argv[1]);
+
+int main(int argc, char **argv) {
+    int i;
+
+    // Check if there are more than 1 command-line arguments
+    if (argc > 1) {
+        // Check if there are more than 2 command-line arguments
+        if (argc > 2)
+            // Print the message 'Thank you' 'n' times based on the second argument
+            for (i = 0; i < argv[2][0] - '0'; i++)
+                printf("Thank you!, %s\n", argv[1]);
+        else
+            // Print 'Greetings' if only one argument is passed
+            printf("Greetings!, %s\n", argv[1]);
     }
     else
-printf("Ola!\n");
+        // If no arguments are passed, print 'Hello'
+        printf("Hello!\n");
+
     return 0;
 }
